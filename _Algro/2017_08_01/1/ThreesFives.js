52657dcb4dc4226adfb3edf29ee52a24d420f432
nickchic <chicirda2@gmail.com>
2017-08-02 14:56:40 -0400

    Complete threeFives algorithm challenge

--- _Algro/2017_08_01/1/ThreesFives.js
+++ _Algro/2017_08_01/1/ThreesFives.js
@@ -1,1 +1,42 @@
-console.log ("hello world");
+threesFives();
+betterThreesFives(100,4000000);
+
+function threesFives() {
+
+    var sum = 0;
+
+    for(var i = 100; i <= 4000000; i++){
+
+        if(i%5 == 0 && i%3 == 0){
+
+        } else if(i%5 == 0 || i%3 == 0){
+
+            sum += i;
+
+        }
+
+    }
+
+    console.log("sum = " + sum);
+
+}
+
+function betterThreesFives(x,y) {
+
+    var sum = 0;
+
+    for(var i = x; i <= y; i++){
+
+        if(i%5 == 0 && i%3 == 0){
+
+        } else if(i%5 == 0 || i%3 == 0){
+
+            sum += i;
+
+        }
+
+    }
+
+    console.log("sum = " + sum);
+
+}
