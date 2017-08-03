c0ef8c5e95d7f5197091128bccc86e10da8635d3
nickchic <chicirda2@gmail.com>
2017-08-03 09:40:22 -0400

    completed Min Sorted Array Algo

---
+++ _Algro/2017_08_01/3/MinSortedArray.js
@@ -0,0 +1,19 @@
+
+
+function findMinInSortedRotated(x) {
+
+    for(var i = 0; i<x.length; i++){
+
+        if(x[i] > x[i+1]){
+
+            return x[i+1];
+
+        }
+
+    }
+
+}
+
+var testArr = ["Gigli","Jay is cool","Mavis","Phoebe","Thurber","Anna","Celeste","Elon"];
+
+console.log(findMinInSortedRotated(testArr));
