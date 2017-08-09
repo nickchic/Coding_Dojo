f75dc8515950e62377ae6dba9f746f820ce98c71
nickchic <chicirda2@gmail.com>
2017-08-09 12:37:16 -0400

    Completed the While You Wait Assignment

---
+++ 1_WebFund/Week2/WhileWait/WhileWait.js
@@ -0,0 +1,26 @@
+var daysUntilBDay = 60;
+
+while(daysUntilBDay > 0){
+
+    if(daysUntilBDay > 30){
+
+        console.log("Aww man its still " + daysUntilBDay + " days until my birthday");
+
+    } else if (daysUntilBDay > 10){
+
+        console.log("Sweet! its " + daysUntilBDay + " days until my birthday");
+
+    } else {
+
+        console.log("OMG My Birthday is only " + daysUntilBDay + " days away");
+
+    }
+
+
+    console.log();
+
+    daysUntilBDay--;
+
+}
+
+console.log("HAPPY BIRTHDAY!!!!");
