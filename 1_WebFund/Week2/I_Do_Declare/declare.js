ddc193c1a01ac23043b9e4a8e9daa5a79d738878
nickchic <chicirda2@gmail.com>
2017-08-09 11:35:11 -0400

    Completed I Do Declare Assignment

---
+++ 1_WebFund/Week2/I_Do_Declare/declare.js
@@ -0,0 +1,29 @@
+var favoriteNumber = 13;
+console.log("Favorite Number = " + favoriteNumber);
+
+var pi = 3.14;
+console.log("pi = " + pi);
+
+var myAge = 30;
+console.log("My Age = " + myAge);
+
+var myAddress = 759;
+console.log("My Address = " + myAddress);
+
+var myName = "Nick";
+console.log("My Name = " + myName);
+
+var today = "Wednesday";
+console.log("Today = " + today);
+
+var favoritePodcast = "The Dollop";
+console.log("Favorite Podcast = " + favoritePodcast);
+
+var thisIsABoolean = true;
+console.log("This Is A Boolean = " + thisIsABoolean);
+
+var thisIsAnHTMLFile = false;
+console.log("This is a HTML file = " + thisIsAnHTMLFile);
+
+var whatever;
+console.log("Whatever = " + whatever);
