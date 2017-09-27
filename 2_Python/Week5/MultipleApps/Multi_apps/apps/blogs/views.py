3b49c3763174fec93fcbafe0d1c895d9a092e16a
nickchic <chicirda2@gmail.com>
2017-09-27 16:29:25 -0400

    Templates and session

--- 2_Python/Week5/MultipleApps/Multi_apps/apps/blogs/views.py
+++ 2_Python/Week5/MultipleApps/Multi_apps/apps/blogs/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     response = "Placeholder to later display all the list of blogs"
-    return HttpResponse(response)
+    return render(request, 'blogs/index.html')
 
 def new(request):
     response = "Placeholder to display a new form to create a new blog"
     return HttpResponse(response)
 
 def create(request):
-    print "Create"
-    return redirect(index)
+    if request.method == "POST":
+    	print "*"*50
+    	print request.POST
+        print request.POST['name']
+        print request.POST['desc']
+        request.session['name'] = request.POST['name']  # more on session below
+    	print "*"*50
+    	return redirect("/")
+    else:
+    	return redirect("/")
 
 def number(request, number):
     response = 'Placeholder to DISPLAY blog {}'.format(number)
