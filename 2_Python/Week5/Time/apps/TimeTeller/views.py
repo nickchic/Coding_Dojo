b260c0ad8fd843a23eb8d07ef4dbde1e0e313e94
nickchic <chicirda2@gmail.com>
2017-09-28 11:01:27 -0400

    Time Display

---
+++ 2_Python/Week5/Time/apps/TimeTeller/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
+
+def index(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request,'TimeTeller/index.html', context)
