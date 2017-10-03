0ea2e257e93e3a7547a7959fe683ba213228a850
nickchic <chicirda2@gmail.com>
2017-10-03 14:34:21 -0400

    Users Django

--- 2_Python/Week6/Survey/Survey/settings.py
+++ 2_Python/Week6/Survey/Survey/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.Users',
     'apps.survey_app',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ 2_Python/Week6/Survey/apps/Users/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def clean(self, *args, **kwargs):
+        if self.first_name == '':
+            raise NameError('Name cannot be blank')
