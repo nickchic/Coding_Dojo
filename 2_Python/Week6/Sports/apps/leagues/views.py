a98d0ca63a4117964ea0ffc848516cddf3e0bc38
nickchic <chicirda2@gmail.com>
2017-10-04 13:52:03 -0400

    Sports 2

--- 2_Python/Week6/Sports/apps/leagues/views.py
+++ 2_Python/Week6/Sports/apps/leagues/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Count
 from .models import League, Team, Player
 
 from . import team_maker
@@ -23,6 +24,21 @@ def index(request):
 		"first_name_joshua": Player.objects.filter(first_name="Joshua"),
 		"cooper_no_joshua": Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua"),
 		"alexander_or_wyatt": Player.objects.filter(first_name="Alexander")|Player.objects.filter(first_name="Wyatt"),
+		"atlantic_soccer_conference_teams": Team.objects.filter(league_id=5),
+		"boston_penguin_players_curr": Player.objects.filter(curr_team_id=28),
+		"ICBC_players_curr": Player.objects.filter(curr_team_id__league_id=2),
+		"ACAF_players_lopez_curr": Player.objects.filter(curr_team_id__league_id=7, last_name='Lopez'),
+		"all_football_players": Player.objects.filter(curr_team_id__league_id__sport='Football'),
+		"teams_with_sophia": Team.objects.filter(curr_players__first_name="Sophia"),
+		"leagues_with_sophia": League.objects.filter(teams__curr_players__first_name="Sophia"),
+		"flores_not_roughriders": Player.objects.filter(last_name="Flores").exclude(curr_team_id__id=10),
+		"samuel_evans_teams": Team.objects.filter(all_players__first_name="Samuel", all_players__last_name="Evans"),
+		"all_tigercats_players": Player.objects.filter(all_teams__id=37),
+		"former_vikings_players": Player.objects.filter(all_teams__id=40).exclude(curr_team__id=40),
+		"jacob_grey_teams_before_colts": Team.objects.filter(all_players__id=151).exclude(curr_players__id=151),
+		"all_joshuas_AFABP": Player.objects.filter(first_name='Joshua', all_teams__league_id=3),
+		"teams_with_tweleve": Team.objects.annotate(all_players_count=Count("all_players")).filter(all_players_count__gt=12).order_by("-all_players_count"),
+		"all_players_with_amount_of_teams": Player.objects.annotate(all_teams_count=Count("all_teams")).order_by("-all_teams_count")
 	}
 	return render(request, "leagues/index.html", context)
 
